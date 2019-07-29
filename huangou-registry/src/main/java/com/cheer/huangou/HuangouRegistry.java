2028285e630c2f80f7f46985dff53f442b9a715f
Teacher Yu <lab.deepmind@gmail.com>
2019-07-29 16:52:59 +0800

    feat(注册中心):

---
+++ huangou-registry/src/main/java/com/cheer/huangou/HuangouRegistry.java
@@ -0,0 +1,13 @@
+package com.cheer.huangou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HuangouRegistry {
+    public static void main(String[] args) {
+        SpringApplication.run(HuangouRegistry.class, args);
+    }
+}
